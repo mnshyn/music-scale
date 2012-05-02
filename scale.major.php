3acdf51affcb7827e2310184e422530403b963af
Cromwel Pestano <mnshyn@gmail.com>
2012-05-02 15:15:16 -0400

    recoded with an interval array to define scale

--- scale.major.php
+++ scale.major.php
@@ -6,7 +6,7 @@
 	circa 05.01.2012
 	*/
 
-	include("scale.php");	// Scale Superclass
+	require_once("scale.php");	// Scale Superclass
 
 	class MajorScale extends Scale
 	{
@@ -21,35 +21,8 @@
 
 		public function createScale($key, $accidental)
 		{
-			$start_info = $this->initializeScale($key, $accidental);
-
-			$note_index = $start_info[0];
-			$notelist = $start_info[1];
-
-			for($c=2; $c<=7; $c++)
-			{
-				switch($c)
-				{
-					case 2:case 4:case 5:case 6:case 7:																																
-					{
-						$new_note = $this->getNextNoteViaSemitones($note_index, 2, $notelist);
-						$note_index+=2;
-
-						$this->insertNote($new_note, $c);
-						break;
-					}
-					case 3:
-					{
-						$new_note = $this->getNextNoteViaSemitones($note_index,	1, $notelist);
-						$note_index+=1;
-
-						$this->insertNote($new_note, $c);
-
-						break;
-					}
-				}
-			}
-
+			$intervals = array(0,0,2,1,2,2,2,2);
+			$this->initializeScale($key, $accidental, $intervals);
 			$this->conformScale();
 		}
 
